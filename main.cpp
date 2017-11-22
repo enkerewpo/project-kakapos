18a610a2d77d7cfd154086c272aa8d7b4ae1bdd9
Kvar_ispw17 <enkerewpo@gmail.com>
2017-11-22 18:46:10 +0800

    Update for linux .

--- main.cpp
+++ main.cpp
@@ -12,12 +12,8 @@ int main(int argc, char *argv[]) {
     splash.setStyleSheet(
                 "color:white;"
                 "background-color:rgb(57,57,57);");
-
-//    splash.show();
     w.show();
     w.move ((QApplication::desktop()->width() - w.width())/2,
                  (QApplication::desktop()->height() - w.height())/2 - 25);
-//    splash.move ((QApplication::desktop()->width() - splash.width())/2,
-//                 (QApplication::desktop()->height() - splash.height())/2 - 25);
     return a.exec();
 }
